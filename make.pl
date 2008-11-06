d7cf2f823b3e323a8effc948f4a6bdd928760a44
miyagawa <miyagawa@65690182-5df0-0310-9871-95da5a3706e7>
2008-11-06 00:21:04 +0000

    link

--- make.pl
+++ make.pl
@@ -99,7 +99,9 @@ sub build_index {
                     a { href is "http://perl.org/"; "Perl" };
                     outs " and ";
                     a { href is "http://search.cpan.org/dist/Text-Kakasi/"; "Text::Kakasi" };
-                    img { src is "http://b.hatena.ne.jp/entry/image/" . "http://blog.bulknews.net/prime_minister_speeches/"; style is "vertical-align:middle" };
+                    my $url = "http://blog.bulknews.net/prime_minister_speeches/";
+                    a { href is "http://b.hatena.ne.jp/entry/$url";
+                        img { src is "http://b.hatena.ne.jp/entry/image/$url" .  style is "vertical-align:middle" } };
                 };
             };
         }
